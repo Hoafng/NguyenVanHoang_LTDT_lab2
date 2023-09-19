ea471399142c16e70bb242abac4648e5eade601c
Hoafng <hoangghcc@gmail.com>
2023-09-19 14:29:54 +0700

    Finish Jquery,XMLHttprequest

--- NguyenVanHoang_LTDT_week2.js
+++ NguyenVanHoang_LTDT_week2.js
@@ -1,25 +1,107 @@
-//Jquery
-const urlBacon = "https://jsonplaceholder.typicode.com/todos/1";
-(function() {
-    $.ajax({
-            method: "GET",
-            url: urlBacon,
-            dataType: "json"
-        })
-        .done(function(data) {
-            console.log(data);
-        })
-        .fail(function() {
-            alert("no good");
-        });
-})()
+// //Jquery
+// const urlBacon = "https://jsonplaceholder.typicode.com/todos/1";
+// (function() {
+//     $.ajax({
+//             method: "GET",
+//             url: urlBacon,
+//             dataType: "json"
+//         })
+//         .done(function(data) {
+//             console.log(data);
+//         })
+//         .fail(function() {
+//             alert("no good");
+//         });
+// })()
 
-//XmlHttpRequest
-//Get all users
-var url = "https://fakestoreapi.com/products/1";
-//var url = 'https://jsonplaceholder.typicode.com/todos';
-var xhr = new XMLHttpRequest()
-xhr.open('GET', url, true)
+// //XmlHttpRequest
+// //Get all users
+// var url = "https://fakestoreapi.com/products/";
+// //var url = 'https://jsonplaceholder.typicode.com/todos';
+// var xhr = new XMLHttpRequest()
+// xhr.open('GET', url, true)
+// xhr.onload = function() {
+//     var users = JSON.parse(xhr.responseText);
+//     if (xhr.readyState == 4 && xhr.status == "200") {
+//         console.table(users);
+//     } else {
+//         console.error(users);
+//     }
+// }
+// xhr.send(null);
+
+//Get a users
+// var url = "https://fakestoreapi.com/products/";
+// //var url = 'https://jsonplaceholder.typicode.com/todos';
+// var xhr = new XMLHttpRequest()
+// xhr.open('GET', url + '/1', true)
+// xhr.onload = function() {
+//     var users = JSON.parse(xhr.responseText);
+//     if (xhr.readyState == 4 && xhr.status == "200") {
+//         console.table(users);
+//     } else {
+//         console.error(users);
+//     }
+// }
+// xhr.send(null);
+
+// Post a user
+// var url = "https://jsonplaceholder.typicode.com/todos/";
+
+// var data = {};
+// data.firstname = "John";
+// data.lastname = "Snow";
+// var json = JSON.stringify(data);
+
+// var xhr = new XMLHttpRequest();
+// xhr.open("POST", url, true);
+// xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+// xhr.onload = function() {
+//     var users = JSON.parse(xhr.responseText);
+//     if (xhr.readyState == 4 && xhr.status == "201") {
+//         console.table(users);
+//     } else {
+//         console.error(users);
+//     }
+// }
+// xhr.send(json);
+
+// Update a user
+// var url = "https://jsonplaceholder.typicode.com/todos/";
+
+// var data = {};
+// data.firstname = "John2";
+// data.lastname = "Snow2";
+// var json = JSON.stringify(data);
+
+// var xhr = new XMLHttpRequest();
+// xhr.open("PUT", url + '/12', true);
+// xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+// xhr.onload = function() {
+//     var users = JSON.parse(xhr.responseText);
+//     if (xhr.readyState == 4 && xhr.status == "200") {
+//         console.table(users);
+//     } else {
+//         console.error(users);
+//     }
+// }
+// xhr.send(json);
+
+// Delete a user
+var url = "https://jsonplaceholder.typicode.com/todos/";
+var xhr = new XMLHttpRequest();
+xhr.open("DELETE", url + '/12', true);
+xhr.onload = function() {
+    var users = JSON.parse(xhr.responseText);
+    if (xhr.readyState == 4 && xhr.status == "200") {
+        console.table(users);
+    } else {
+        console.error(users);
+    }
+}
+xhr.send(null);
+
+xhr.open('GET', url + '/12', true)
 xhr.onload = function() {
     var users = JSON.parse(xhr.responseText);
     if (xhr.readyState == 4 && xhr.status == "200") {
